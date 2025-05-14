a2706781c2a2691c30ad6b6f9679c8669dd8a489
mgdtk <marlon.destro@hotmail.com>
2025-05-13 22:40:56 -0300

    feat: Add window click-through functionality

--- screen_filter.py
+++ screen_filter.py
@@ -1,9 +1,24 @@
 import tkinter as tk
+from win32gui import SetWindowLong, GetWindowLong, SetLayeredWindowAttributes
+from win32con import WS_EX_LAYERED, WS_EX_TRANSPARENT, GWL_EXSTYLE
 
-window = tk.Tk()
+def setClickThrough(hwnd):
+    try:
+        styles = GetWindowLong(hwnd, GWL_EXSTYLE)
+        styles = WS_EX_LAYERED | WS_EX_TRANSPARENT
+        SetWindowLong(hwnd, GWL_EXSTYLE, styles)
+        SetLayeredWindowAttributes(hwnd, 0, 255, 0x00000001)
+    except Exception as e:
+        print(e)
 
-window.attributes('-fullscreen', True)
-window.attributes('-alpha', 0.3)
-window['bg'] = '#FBF0D9'
+if __name__ == "__main__":
+    window = tk.Tk()
+    
+    window.attributes('-fullscreen', True)
+    window['bg'] = '#FBF0D9'
+    window.attributes('-alpha', 0.3)
+    window.attributes('-transparentcolor', '#000000', '-topmost', 1)
 
-window.mainloop()
+    setClickThrough(window.winfo_id())
+
+    window.mainloop()
