7c67e1b83826971981e019307f2ac673134ca449
mgdtk <marlon.destro@hotmail.com>
2025-05-19 17:55:30 -0300

    refactor: enhance window behavior and error handling
    - Add window.overrideredirect(True) to remove window display in the taskbar.
    - Remove check=True from subprocess.run() as it was causing unnecessary CalledProcessError exceptions for explorer commands, which already provide sufficient error information.

--- screen_filter.py
+++ screen_filter.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     window['bg'] = '#FBF0D9'
     window.attributes('-alpha', 0.3)
     window.attributes('-transparentcolor', '#000000', '-topmost', 1)
+    window.overrideredirect(True)
 
     setClickThrough(window.winfo_id())
 
@@ -35,11 +36,8 @@ if __name__ == "__main__":
     print(f'Attempting to launch Aquile Reader using command: {command_to_run}')
 
     try:
-        subprocess.run(command_to_run, shell=True, check=True, capture_output=True, text=True)
+        subprocess.run(command_to_run, shell=True, capture_output=True, text=True)
         print('Aquile Reader launched successfully.')
-    except subprocess.CalledProcessError as e:
-        print(f'Error executing command: {e}')
-        print(f'Stderr: {e.stderr}')
     except FileNotFoundError:
         print("Error: 'explorer' command not found")
     except Exception as e:
